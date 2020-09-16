4616bf1339265465c5ca122af3200f440c957b74
Mat McLoughlin <mat@poweredbybeard.net>
2020-09-16 16:21:41 +0100

    Moving to http

--- Application/Program.cs
+++ Application/Program.cs
@@ -46,17 +46,9 @@ namespace Application
             {
                 ConnectivitySettings =
                 {
-                    Address = new Uri("https://localhost:2113"),
+                    Address = new Uri("http://localhost:2113"),
                 },
-                DefaultCredentials = new UserCredentials("admin", "changeit"),
-                CreateHttpMessageHandler = () =>
-                    new SocketsHttpHandler
-                    {
-                        SslOptions =
-                        {
-                            RemoteCertificateValidationCallback = delegate { return true; }
-                        }
-                    }
+                DefaultCredentials = new UserCredentials("admin", "changeit")
             });
         }
     }
