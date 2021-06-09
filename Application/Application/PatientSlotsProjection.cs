4d9b8c328d9aa5c99c68d33a392b90646f48d12b
Kacper Gunia <kacper@gunia.me>
2021-06-09 19:48:50 +0100

    Add patients slots projection plumbing

---
+++ Application/Application/PatientSlotsProjection.cs
@@ -0,0 +1,15 @@
+using System.Threading.Tasks;
+using Application.Domain.ReadModel;
+using Application.Domain.WriteModel.Events;
+using Application.Infrastructure.Projections;
+
+namespace Application.Application
+{
+    public class PatientSlotsProjection : Projection
+    {
+        public PatientSlotsProjection(IPatientSlotsRepository repo)
+        {
+        }
+    }
+
+}
