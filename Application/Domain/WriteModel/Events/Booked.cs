9121e8755602d641b28223299f7656bdb4605943
Oskar Dudycz <oskar.dudycz@gmail.com>
2021-11-30 11:35:59 +0100

    Changed Commands and Events into record. Changed Command and Event from classes to interfaces and renamed with `I` prefix accordingly

--- Application/Domain/WriteModel/Events/Booked.cs
+++ Application/Domain/WriteModel/Events/Booked.cs
@@ -1,25 +1,8 @@
-using System;
 using Application.EventSourcing;
 
 namespace Application.Domain.WriteModel.Events;
 
-
-/*
-protected bool Equals(Booked other)
-{
-    return SlotId.Equals(other.SlotId) && SlotId.Equals(other.SlotId) && PatientId == other.PatientId;
-}
-
-public override bool Equals(object obj)
-{
-    if (ReferenceEquals(null, obj)) return false;
-    if (ReferenceEquals(this, obj)) return true;
-    if (obj.GetType() != this.GetType()) return false;
-    return Equals((Booked) obj);
-}
-
-public override int GetHashCode()
-{
-    return HashCode.Combine(SlotId, PatientId);
-}
- */
+public record Booked(
+    string SlotId,
+    string PatientId
+): IEvent;
--- Application/Domain/WriteModel/Events/Cancelled.cs
+++ Application/Domain/WriteModel/Events/Cancelled.cs
@@ -1,25 +1,8 @@
-using System;
 using Application.EventSourcing;
 
 namespace Application.Domain.WriteModel.Events;
 
-
-/*
-protected bool Equals(Cancelled other)
-{
-    return SlotId.Equals(other.SlotId) && Reason == other.Reason;
-}
-
-public override bool Equals(object obj)
-{
-    if (ReferenceEquals(null, obj)) return false;
-    if (ReferenceEquals(this, obj)) return true;
-    if (obj.GetType() != this.GetType()) return false;
-    return Equals((Cancelled) obj);
-}
-
-public override int GetHashCode()
-{
-    return HashCode.Combine(SlotId, Reason);
-}
- */
+public record Cancelled(
+    string SlotId,
+    string Reason
+): IEvent;
